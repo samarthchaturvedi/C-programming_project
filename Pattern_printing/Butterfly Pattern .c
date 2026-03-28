ba1ce99a77db8f916342e9850abaf0d3269b585f
Samarth Chaturvedi <samarthchaturvedi@jklu.edu.in>
2026-03-28 18:45:34 +0530

    Add files via upload

---
+++ Pattern_printing/Butterfly Pattern .c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter rows: ");
+    scanf("%d", &n);
+
+    // upper half
+    for(int i = 1; i <= n; i++) {
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        for(int j = 1; j <= 2*(n-i); j++)
+            printf(" ");
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    // lower half
+    for(int i = n; i >= 1; i--) {
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        for(int j = 1; j <= 2*(n-i); j++)
+            printf(" ");
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Pattern_printing/Full Pyramid .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter rows: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+
+        // spaces
+        for(int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // stars
+        for(int j = 1; j <= (2*i - 1); j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
